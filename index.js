cbc57a4e15b7f8f9950d993428bb57a09d4fdfd0
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 09:26:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
     kittens.push(name);
-    return kittens;
+    // return kittens;
 }
 
 function destructivelyPrependKitten(name) {
     kittens.unshift(name);
-    return kittens;
+    // return kittens;
 }
